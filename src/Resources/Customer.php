37d6b8b06d1dc09868d62b0b65f268f0988f6bb8
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2024-05-04 12:38:22 -0600

    feat(customer): add method to get by CFDI or UID

--- src/Resources/Customer.php
+++ src/Resources/Customer.php
@@ -56,4 +56,49 @@ class Customer extends BaseCilent
         }
         return $ret;
     }
+
+    /**
+     * Retrieves a customer object by its RFC or UID from the API.
+     *
+     * @param string $id The RFC or UID of the customer to retrieve.
+     * @throws FacturaComException If the API response status is not "success".
+     * @return Types\Customer The customer object with the specified RFC or UID.
+     */
+    public function get_by_id(string $id)
+    {
+        $response = json_decode($this->get([$id])->getBody(), true);
+        if ($response["status"] != "success") {
+            throw new FacturaComException($response["message"]);
+        }
+        $data = $response["Data"];
+        return new Types\Customer(
+            $data["UID"],
+            $data["RazonSocial"],
+            $data["RFC"],
+            $data["Regimen"],
+            $data["RegimenId"],
+            $data["Calle"],
+            $data["Numero"],
+            $data["Interior"],
+            $data["Colonia"],
+            $data["CodigoPostal"],
+            $data["Ciudad"],
+            $data["Delegacion"],
+            $data["Estado"],
+            $data["Localidad"],
+            $data["Pais"],
+            $data["NumRegIdTrib"],
+            $data["UsoCFDI"],
+            new Types\Contact(
+                $data["Contacto"]["Nombre"],
+                $data["Contacto"]["Apellidos"],
+                $data["Contacto"]["Email"],
+                $data["Contacto"]["Email2"],
+                $data["Contacto"]["Email3"],
+                $data["Contacto"]["Telefono"]
+            ),
+            $data["cfdis"],
+            $data["cuentas_banco"]
+        );
+    }
 }
