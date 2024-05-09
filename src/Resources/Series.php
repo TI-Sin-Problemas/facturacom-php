60c5ef2d9c9de541f836ec929c9142f7de33bcd1
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2024-05-08 23:52:13 -0600

    feat(series): add method to create new series

--- src/Resources/Series.php
+++ src/Resources/Series.php
@@ -4,9 +4,11 @@ namespace TiSinProblemas\FacturaCom\Resources;
 
 require_once __DIR__ . '/../Types/Series.php';
 
+use ReflectionClass;
 use TiSinProblemas\FacturaCom\Exceptions\FacturaComException;
 use TiSinProblemas\FacturaCom\Http\BaseCilent;
 use TiSinProblemas\FacturaCom\Types;
+use TiSinProblemas\FacturaCom\Constants;
 
 class Series extends BaseCilent
 {
@@ -16,6 +18,16 @@ class Series extends BaseCilent
     private function execute_get_request(array $url_params, array $query_params = null)
     {
         $response = $this->get($url_params, $query_params);
+        $data = json_decode($response->getBody(), true);
+        if ($data["status"] != "success") {
+            throw new FacturaComException($data["message"]);
+        }
+        return $data;
+    }
+
+    private function execute_post_request(array $url_params, array $data)
+    {
+        $response = $this->post($url_params, $data);
         $data = json_decode($response->getBody(), true);
         if ($data["status"] != "success") {
             throw new FacturaComException($data["message"]);
@@ -59,5 +71,35 @@ class Series extends BaseCilent
             $response["data"]["SerieDescription"],
             $response["data"]["SerieStatus"]
         );
+    }
+
+    /**
+     * Creates a new series with the given letter, document type, and optional folio.
+     *
+     * @param string $letter The letter of the series. With this parameter you can define a
+     *                        letter as an identifier for the series.
+     * @param Constants\DocumentType $document_type The type of CFDI or document that corresponds
+     *                               to the series.
+     * @param int|null $folio It is used to define a custom number to start the counter for the
+     *                        series(optional).
+     * @throws FacturaComException If the document type is invalid.
+     * @return string The message from the response.
+     */
+    public function create(string $letter, Constants\DocumentType $document_type, int $folio = null): string
+    {
+        $document_type_reflection = new ReflectionClass(Constants\DocumentType::class);
+        $valid_document_types = $document_type_reflection->getConstants();
+        if (!in_array($document_type, $valid_document_types)) {
+            throw new FacturaComException("Invalid document type. Valid types are: " . implode(", ", $valid_document_types));
+        }
+
+        $data = [
+            "letra" => $letter,
+            "tipoDocumento" => $document_type,
+            "folio" => $folio
+        ];
+
+        $response =  $this->execute_post_request(["create"], $data);
+        return $response["message"];
     }
 }
